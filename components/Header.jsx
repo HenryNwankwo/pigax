6087c345d944173bf98cb59bb4016273426e52f2
Nwankwo Henry <nameisifunanya@gmail.com>
2023-09-18 18:57:44 +0100

    Styled the LOgo group, menu burger group, and search group

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,28 @@
+'use client';
 import Link from 'next/link';
+import Image from 'next/image';
+import { RiMenu4Line, RiCloseLine, RiSearchLine } from 'react-icons/ri';
+import { useState } from 'react';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <section className='pgx-header'>
       <div className='pgx-logo-group'>
         <Link href='/'>Pigax</Link>
       </div>
-      <div>
+      <div className='pgx-search-login-group'>
         <article className='pgx-search-group'>
-          <input type='text' name='search' id='search' />
-          <label htmlFor='search'></label>
+          <input
+            type='text'
+            name='search'
+            id='search'
+            placeholder='Search for images...'
+            className='pgx-search-input'
+          />
+          <label htmlFor='search' className='pgx-search-icon'>
+            <RiSearchLine className='text-orange-500 text-2xl'></RiSearchLine>
+          </label>
         </article>
         <article className='pgx-image-login-group'>
           <div className='pgx-image-group'>
@@ -29,6 +42,13 @@ function Header() {
             Login
           </button>
         </article>
+      </div>
+      <div className='pgx-menu-burger'>
+        {isMenuOpen ? (
+          <RiCloseLine className='text-3xl text-orange-500'></RiCloseLine>
+        ) : (
+          <RiMenu4Line className='text-3xl text-orange-500'></RiMenu4Line>
+        )}
       </div>
     </section>
   );
