98d07c37e4c1e194e57c968e30467479f9e4bda7
Nwankwo Henry <nameisifunanya@gmail.com>
2023-09-20 18:47:21 +0100

    Added react-toastify

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
+import { ToastContainer } from 'react-toastify';
 import { PigaxProvider } from './PigaxContext';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <PigaxProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <ToastContainer newestOnTop={true} />
+        </body>
       </PigaxProvider>
     </html>
   );
