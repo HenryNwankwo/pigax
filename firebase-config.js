312d8b3f6347af89050432804a6d6bf082f03990
Nwankwo Henry <nameisifunanya@gmail.com>
2023-09-20 09:32:32 +0100

    Created a firebase config file

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB_9qnKg2lLWFbxFAYpOGrhxHwdeKfXtYM',
+  authDomain: 'pigax-40ba4.firebaseapp.com',
+  projectId: 'pigax-40ba4',
+  storageBucket: 'pigax-40ba4.appspot.com',
+  messagingSenderId: '248476328586',
+  appId: '1:248476328586:web:d95635208327e1f7484373',
+  measurementId: 'G-VWDEX0SKT0',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
